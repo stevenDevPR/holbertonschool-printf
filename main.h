ab43cc2166e9081b0ddf394e123cce3d34340f2e
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-25 13:06:55 -0400

    makingt the thinging

--- main.h
+++ main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _printf(const char *format, ...);
-int _putchar(char c);
 
+#endif /* MAIN_H */
 
-
-#endif
---
+++ printf.c
@@ -0,0 +1,73 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    int len = 0;
+  unsigned int ui;
+
+    va_start(args, format);
+    while (format && format[count])
+    {
+        if (format[count] == '%')
+        {
+            count++;
+            switch (format[count])
+            {
+                case 'd':
+                case 'i':
+                    len += printf("%d", va_arg(args, int));
+                    break;
+                case 'u':
+                    len += printf("%u", va_arg(args, unsigned int));
+                    break;
+                case 'o':
+                    len += printf("%o", va_arg(args, unsigned int));
+                    break;
+                case 'x':
+                    len += printf("%x", va_arg(args, unsigned int));
+                    break;
+                case 'X':
+                    len += printf("%X", va_arg(args, unsigned int));
+                    break;
+                case 'c':
+                    len += printf("%c", va_arg(args, int));
+                    break;
+                case 's':
+                    len += printf("%s", va_arg(args, char *));
+                    break;
+                case 'p':
+                    len += printf("%p", va_arg(args, void *));
+                    break;
+                case '%':
+                    putchar('%');
+                    len++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(format[count]);
+                    len += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(format[count]);
+            len++;
+        }
+        count++;
+    }
+    va_end(args);
+
+    return len;
+}
