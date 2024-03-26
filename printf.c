ffaab711e197565cb179f7f46548d5ce8c8ffe27
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-03-26 09:54:36 -0400

    making the printf jajaj puting something on the if

--- printf.c
+++ printf.c
@@ -11,63 +11,67 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int len = 0;
-  unsigned int ui;
+	va_list args;
+	int len = 0, count = 0, ui;
+	va_start(args, format);
+	
+	if (!format)
+	{
+		return (-1);
+	}
 
-    va_start(args, format);
-    while (format && format[count])
-    {
-        if (format[count] == '%')
-        {
-            count++;
-            switch (format[count])
-            {
-                case 'd':
-                case 'i':
-                    len += printf("%d", va_arg(args, int));
-                    break;
-                case 'u':
-                    len += printf("%u", va_arg(args, unsigned int));
-                    break;
-                case 'o':
-                    len += printf("%o", va_arg(args, unsigned int));
-                    break;
-                case 'x':
-                    len += printf("%x", va_arg(args, unsigned int));
-                    break;
-                case 'X':
-                    len += printf("%X", va_arg(args, unsigned int));
-                    break;
-                case 'c':
-                    len += printf("%c", va_arg(args, int));
-                    break;
-                case 's':
-                    len += printf("%s", va_arg(args, char *));
-                    break;
-                case 'p':
-                    len += printf("%p", va_arg(args, void *));
-                    break;
-                case '%':
-                    putchar('%');
-                    len++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[count]);
-                    len += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(format[count]);
-            len++;
-        }
-        count++;
-    }
-    va_end(args);
+	while (format && format[count])
+	{
+		if (format[count] == '%')
+		{
+			count++;
+			switch (format[count])
+			{
+				case 'd':
+				case 'i':
+					len += printf("%d", va_arg(args, int));
+					break;
+				case 'u':
+					ui = va_arg(args, unsigned int);
+					len += printf("%u", ui);
+					break;
+				case 'o':
+					len += printf("%o", va_arg(args, unsigned int));
+					break;
+				case 'x':
+					len += printf("%x", va_arg(args, unsigned int));
+					break;
+				case 'X':
+					len += printf("%X", va_arg(args, unsigned int));
+					break;
+				case 'c':
+					len += printf("%c", va_arg(args, int));
+					break;
+				case 's':
+					len += printf("%s", va_arg(args, char *));
+					break;
+				case 'p':
+					len += printf("%p", va_arg(args, void *));
+					break;
+				case '%':
+					putchar('%');
+					len++;
+					break;
+				default:
+					putchar('%');
+					putchar(format[count]);
+					len += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[count]);
+			len++;
+		}
+		count++;
+	}
+	va_end(args);
 
-    return len;
+return len;
 }
