7b1491dbdcbec2644ae342fcaef9fa815cec49c4
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-04 10:28:28 -0400

    fixing comment C99

--- printf.c
+++ printf.c
@@ -9,42 +9,42 @@
  */
 int _printf(const char *format, ...)
 {
-	int len = 0; //len nos va sumando cantidad de caracteres
-	va_list args; //variable de los argumentos
+	int len = 0;
+	va_list args;
 
-	va_start(args, format); //se inicializa variable de argumentos
+	va_start(args, format);
 
-	if (!format) //verificamos que el string format no es nulo
-		return (-1); //si es NULL retorna -1
+	if (!format)
+		return (-1);
 
-	while (*format) //loop donde iteramos en cada carácter en el string format
+	while (*format)
 	{
-		if (*format == '%') //se verifica si el caracter es %
+		if (*format == '%')
 		{
-			if (*(format + 1) == '%') //se verifica siguiente caracter al lado
+			if (*(format + 1) == '%')
 			{
-				len += print_percent(); //se imprime literal el signo %
-				format += 2; //hacemos skip al signo de porciento
+				len += print_percent();
+				format += 2;
 				continue;
 			}
-			else if (*(format + 1) == '\0') //verificamos que no sea NULL luego del primer %
+			else if (*(format + 1) == '\0')
 			{
-				va_end(args); //si es NULL liberamos args
-				return (-1); //retorna -1 format specifier incompleto
+				va_end(args);
+				return (-1);
 			}
-			else //si no es NULL o no es el signo de porciento haz esto
+			else
 			{
-				format++; //nos movemos luego del signo %
-				len += handle_format(format, args);//utilizamos funcion handle_format pra deteminar 
+				format++;
+				len += handle_format(format, args); 
 			}
 		}
-		else //si al iterar no hay % incial
+		else
 		{
-			print_char(*format); // se imprime el carácter normal
-			len++; //incrementamos len
+			print_char(*format);
+			len++;
 		}
-		format++; //nos movemos the caracter en format string
+		format++;
 	}
-	va_end(args); //liberamos args
-	return (len); //se retorna la cantidad total de caracteres
+	va_end(args);
+	return (len);
 }
