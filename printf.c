3d6a05ba0a1fc36348d985be1bb13624a9ef7acd
Steven Bonilla <110514800+stevenDevPR@users.noreply.github.com>
2024-04-04 08:07:28 -0400

    Explanation of printf
    Adding comments for better understanding

--- printf.c
+++ printf.c
@@ -9,42 +9,42 @@
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	va_list args;
+	int len = 0; //len nos va sumando cantidad de caracteres
+	va_list args; //variable de los argumentos
 
-	va_start(args, format);
+	va_start(args, format); //se inicializa variable de argumentos
 
-	if (!format)
-		return (-1);
+	if (!format) //verificamos que el string format no es nulo
+		return (-1); //si es NULL retorna -1
 
-	while (*format)
+	while (*format) //loop donde iteramos en cada carácter en el string format
 	{
-		if (*format == '%')
+		if (*format == '%') //se verifica si el caracter es %
 		{
-			if (*(format + 1) == '%')
+			if (*(format + 1) == '%') //se verifica siguiente caracter al lado
 			{
-				len += print_percent();
-				format += 2;
+				len += print_percent(); //se imprime literal el signo %
+				format += 2; //hacemos skip al signo de porciento
 				continue;
 			}
-			else if (*(format + 1) == '\0')
+			else if (*(format + 1) == '\0') //verificamos que no sea NULL luego del primer %
 			{
-				va_end(args);
-				return (-1);
+				va_end(args); //si es NULL liberamos args
+				return (-1); //retorna -1 format specifier incompleto
 			}
-			else
+			else //si no es NULL o no es el signo de porciento haz esto
 			{
-				format++;
-				len += handle_format(format, args);
+				format++; //nos movemos luego del signo %
+				len += handle_format(format, args);//utilizamos funcion handle_format pra deteminar 
 			}
 		}
-		else
+		else //si al iterar no hay % incial
 		{
-			print_char(*format);
-			len++;
+			print_char(*format); // se imprime el carácter normal
+			len++; //incrementamos len
 		}
-		format++;
+		format++; //nos movemos the caracter en format string
 	}
-	va_end(args);
-	return (len);
+	va_end(args); //liberamos args
+	return (len); //se retorna la cantidad total de caracteres
 }
